98ef525464da5ec6db868a33c2c81a8748a0f802
WarLikeLaux <warlikelaux080399@mail.ru>
2023-11-12 19:49:45 +0600

    Added Pokemon.image model field

---
+++ pokemon_entities/migrations/0002_pokemon_image.py
@@ -0,0 +1,16 @@
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('pokemon_entities', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='pokemon',
+            name='image',
+            field=models.ImageField(blank=True, null=True, upload_to='pokemons'),
+        ),
+    ]
