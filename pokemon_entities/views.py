8a6f0c9adf6b2e46a493806fa725487778f69f76
WarLikeLaux <warlikelaux080399@mail.ru>
2023-11-13 23:05:23 +0600

    Added using shortcut get_object_or_404

--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -1,8 +1,6 @@
 import folium
 
-from django.core.exceptions import ObjectDoesNotExist
-from django.http import HttpResponseNotFound
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 
 from .models import Pokemon, PokemonEntity
@@ -56,10 +54,7 @@ def show_all_pokemons(request):
 
 
 def show_pokemon(request, pokemon_id):
-    try:
-        pokemon = Pokemon.objects.get(id=pokemon_id)
-    except ObjectDoesNotExist:
-        return HttpResponseNotFound('<h1>Такой покемон не найден</h1>')
+    pokemon = get_object_or_404(Pokemon, id=pokemon_id)
 
     pokemon_entites = pokemon.entities.all()
     folium_map = folium.Map(location=MOSCOW_CENTER, zoom_start=12)
