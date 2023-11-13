6eb9acf740c5d327731fea151cc352a7e798606f
WarLikeLaux <warlikelaux080399@mail.ru>
2023-11-13 22:24:48 +0600

    Added related_names for models

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -11,7 +11,14 @@ class Pokemon(models.Model):
     title_jp = models.CharField("Название (япон.)", max_length=200, blank=True)
     image = models.ImageField("Изображение", upload_to='pokemons')
     description = models.TextField("Описание", blank=True)
-    previous_evolution = models.ForeignKey('self', verbose_name="Предыдущая эволюция", on_delete=models.CASCADE, null=True, blank=True)
+    previous_evolution = models.ForeignKey(
+        'self',
+        verbose_name="Предыдущая эволюция",
+        related_name="next_evolutions",
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True
+    )
 
     def __str__(self):
         return self.title
@@ -24,7 +31,12 @@ def default_disappeared_at():
 
 
 class PokemonEntity(models.Model):
-    pokemon = models.ForeignKey(Pokemon, verbose_name="Покемон", on_delete=models.CASCADE)
+    pokemon = models.ForeignKey(
+        Pokemon,
+        verbose_name="Покемон",
+        related_name="entities",
+        on_delete=models.CASCADE
+    )
     lat = models.FloatField("Широта")
     lon = models.FloatField("Долгота")
     appeared_at = models.DateTimeField("Время появления", default=timezone.now)
