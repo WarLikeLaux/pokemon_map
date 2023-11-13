1e4d91ecc2c911c15a3eeb2d94565e6b93c8f4bf
WarLikeLaux <warlikelaux080399@mail.ru>
2023-11-13 22:21:08 +0600

    Hidden empty default in text fields

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -7,10 +7,10 @@ from dotenv import load_dotenv
 
 class Pokemon(models.Model):
     title = models.CharField("Название", max_length=200)
-    title_en = models.CharField("Название (англ.)", max_length=200, default='', blank=True)
-    title_jp = models.CharField("Название (япон.)", max_length=200, default='', blank=True)
+    title_en = models.CharField("Название (англ.)", max_length=200, blank=True)
+    title_jp = models.CharField("Название (япон.)", max_length=200, blank=True)
     image = models.ImageField("Изображение", upload_to='pokemons')
-    description = models.TextField("Описание", default='', blank=True)
+    description = models.TextField("Описание", blank=True)
     previous_evolution = models.ForeignKey('self', verbose_name="Предыдущая эволюция", on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
